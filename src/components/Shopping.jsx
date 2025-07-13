2c108344baaf61639c649eb48f1ca8663d687ca2
aliou-prog <alimokhati2023@gmail.com>
2025-07-13 19:45:56 +0100

    Cart total

--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 function Shopping() {
     const [data, set_data] = useState(null);
+    const [cart_total, set_cart_total] = useState(0);
     useEffect(() => {
         async function fetch_data() {
             const response = await fetch('https://fakestoreapi.com/products');
@@ -13,24 +14,30 @@ function Shopping() {
     return (
         <>
             <h1>Shopping Page</h1>
-            <div className="cart">Cart</div>
-            <div className="cards">{data && data.map(item => <Card key={item.id} item={item} />)}</div>
+            <div className="cart">Cart: {cart_total.toFixed(2)}</div>
+            <div className="cards">{data && data.map(item => <Card key={item.id} item={item} set_cart_total={set_cart_total}/>)}</div>
         </>)
 }
 
-function Card({ item }) {
-    const [count, setCount] = useState(0)
+function Card({ item , set_cart_total}) {
+    const [count, setCount] = useState(0);
+    const item_total = parseFloat((count * item.price).toFixed(2))
+    function add_to_cart(){
+        set_cart_total(prev => prev + item_total );
+        setCount(0);
+    }
     return (
         <>
             <div className="card">
                 <img src={item.image} alt='card img'></img>
                 <div className='control'>
-                    <div className="total">total: {(count * item.price).toFixed(2)}</div>
+                    <div className="total">total: {item_total}</div>
                     <div className="count">{count}</div>
                     <div className="btns">
                         <button className="inc" onClick={() => setCount(prev => prev + 1)}>inc</button>
                         <button className="dec" onClick={() => setCount(prev => prev > 0 ? prev - 1 : 0)}>dec</button>
                     </div>
+                    <button className="add-to-cart" onClick={add_to_cart}>Add to cart</button>
                 </div>
             </div>
         </>
